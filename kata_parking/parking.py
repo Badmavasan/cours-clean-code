b9970138c17e04fa1914f799c7d1c1312bfa8d7d
Badmavasan KIROUCHENASSAMY <sunshinevasan@gmail.com>
2026-09-15 10:46:00 +0200

    refactor: extraction du calcul des tranches et du plafond

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -7,8 +7,15 @@ PLAFOND_PAR_JOURNEE = 18.00
 MINUTES_PAR_JOURNEE = 24 * 60
 
 
-def tarif(duree_en_minutes):
+def _montant_des_tranches(duree_en_minutes):
     minutes_facturables = max(0, duree_en_minutes - MINUTES_GRATUITES_STANDARD)
-    montant = math.ceil(minutes_facturables / MINUTES_PAR_TRANCHE) * TARIF_PAR_TRANCHE
+    return math.ceil(minutes_facturables / MINUTES_PAR_TRANCHE) * TARIF_PAR_TRANCHE
+
+
+def _plafond(duree_en_minutes):
     journees = max(1, math.ceil(duree_en_minutes / MINUTES_PAR_JOURNEE))
-    return min(montant, journees * PLAFOND_PAR_JOURNEE)
+    return journees * PLAFOND_PAR_JOURNEE
+
+
+def tarif(duree_en_minutes):
+    return min(_montant_des_tranches(duree_en_minutes), _plafond(duree_en_minutes))

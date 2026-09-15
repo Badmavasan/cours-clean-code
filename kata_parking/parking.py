366b03f5b89ea957dd099783dd2940188f450d6d
Badmavasan KIROUCHENASSAMY <sunshinevasan@gmail.com>
2026-09-15 11:34:00 +0200

    green: une sortie anterieure a l'entree est refusee

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -48,5 +48,7 @@ def tarif(duree_en_minutes, est_abonne=False, est_electrique=False):
 
 
 def tarif_en_cours(entree, maintenant, est_abonne=False, est_electrique=False):
+    if maintenant < entree:
+        raise DureeInvalide("la sortie est anterieure a l'entree")
     minutes = int((maintenant - entree).total_seconds() // 60)
     return tarif(minutes, est_abonne, est_electrique)

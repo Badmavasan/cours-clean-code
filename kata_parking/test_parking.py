b5f2c051ad696d103406751dafd338c3b67fc3ab
Badmavasan KIROUCHENASSAMY <sunshinevasan@gmail.com>
2026-09-15 10:12:00 +0200

    test: toute duree jusqu'a trente minutes est gratuite

--- kata_parking/test_parking.py
+++ kata_parking/test_parking.py
@@ -1,5 +1,11 @@
+import pytest
 from parking import tarif
 
 
 def test_un_stationnement_de_trente_minutes_est_gratuit():
     assert tarif(30) == 0.00
+
+
+@pytest.mark.parametrize("minutes", [0, 1, 15, 29, 30])
+def test_toute_duree_jusqu_a_trente_minutes_est_gratuite(minutes):
+    assert tarif(minutes) == 0.00

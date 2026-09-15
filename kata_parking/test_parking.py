03e5615a08995c4b4bc04f04c177a739fbdf694f
Badmavasan KIROUCHENASSAMY <sunshinevasan@gmail.com>
2026-09-15 10:15:00 +0200

    red: la trente et unieme minute coute un euro cinquante

--- kata_parking/test_parking.py
+++ kata_parking/test_parking.py
@@ -9,3 +9,7 @@ def test_un_stationnement_de_trente_minutes_est_gratuit():
 @pytest.mark.parametrize("minutes", [0, 1, 15, 29, 30])
 def test_toute_duree_jusqu_a_trente_minutes_est_gratuite(minutes):
     assert tarif(minutes) == 0.00
+
+
+def test_la_trente_et_unieme_minute_coute_un_euro_cinquante():
+    assert tarif(31) == 1.50

79470cb555542958b4ff163882cdf9f8cfc15dba
Badmavasan KIROUCHENASSAMY <sunshinevasan@gmail.com>
2026-09-15 10:33:00 +0200

    green: au dela de six heures trente le montant est plafonne

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -3,8 +3,10 @@ import math
 MINUTES_GRATUITES_STANDARD = 30
 MINUTES_PAR_TRANCHE = 30
 TARIF_PAR_TRANCHE = 1.50
+PLAFOND_PAR_JOURNEE = 18.00
 
 
 def tarif(duree_en_minutes):
     minutes_facturables = max(0, duree_en_minutes - MINUTES_GRATUITES_STANDARD)
-    return math.ceil(minutes_facturables / MINUTES_PAR_TRANCHE) * TARIF_PAR_TRANCHE
+    montant = math.ceil(minutes_facturables / MINUTES_PAR_TRANCHE) * TARIF_PAR_TRANCHE
+    return min(montant, PLAFOND_PAR_JOURNEE)

18887831a899bff4f7d09942d0812752c0f5d5c4
Badmavasan KIROUCHENASSAMY <sunshinevasan@gmail.com>
2026-09-15 11:08:00 +0200

    refactor: extraction de la duree gratuite

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,6 +1,7 @@
 import math
 
 MINUTES_GRATUITES_STANDARD = 30
+MINUTES_GRATUITES_VEHICULE_ELECTRIQUE = 60
 MINUTES_PAR_TRANCHE = 30
 TARIF_PAR_TRANCHE = 1.50
 PLAFOND_PAR_JOURNEE = 18.00
@@ -8,9 +9,16 @@ MINUTES_PAR_JOURNEE = 24 * 60
 PART_PAYEE_PAR_UN_ABONNE = 0.60
 
 
+def _minutes_gratuites(est_electrique):
+    if est_electrique:
+        return MINUTES_GRATUITES_VEHICULE_ELECTRIQUE
+    return MINUTES_GRATUITES_STANDARD
+
+
 def _montant_des_tranches(duree_en_minutes, est_electrique):
-    gratuites = 60 if est_electrique else MINUTES_GRATUITES_STANDARD
-    minutes_facturables = max(0, duree_en_minutes - gratuites)
+    minutes_facturables = max(
+        0, duree_en_minutes - _minutes_gratuites(est_electrique)
+    )
     return math.ceil(minutes_facturables / MINUTES_PAR_TRANCHE) * TARIF_PAR_TRANCHE
 
 

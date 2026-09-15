c1f2bec967ace2b6dd83ee16e209d9613bc087d7
Badmavasan KIROUCHENASSAMY <sunshinevasan@gmail.com>
2026-09-15 10:28:00 +0200

    refactor: constantes nommees a la place des nombres en dur

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,6 +1,10 @@
 import math
 
+MINUTES_GRATUITES_STANDARD = 30
+MINUTES_PAR_TRANCHE = 30
+TARIF_PAR_TRANCHE = 1.50
+
 
 def tarif(duree_en_minutes):
-    minutes_facturables = max(0, duree_en_minutes - 30)
-    return math.ceil(minutes_facturables / 30) * 1.50
+    minutes_facturables = max(0, duree_en_minutes - MINUTES_GRATUITES_STANDARD)
+    return math.ceil(minutes_facturables / MINUTES_PAR_TRANCHE) * TARIF_PAR_TRANCHE

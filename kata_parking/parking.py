2fa0047ab3c0348dbb6e9710c0ed00ec3ced3292
Badmavasan KIROUCHENASSAMY <sunshinevasan@gmail.com>
2026-09-15 10:40:00 +0200

    green: le plafond vaut dix-huit euros par journee commencee

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -4,9 +4,11 @@ MINUTES_GRATUITES_STANDARD = 30
 MINUTES_PAR_TRANCHE = 30
 TARIF_PAR_TRANCHE = 1.50
 PLAFOND_PAR_JOURNEE = 18.00
+MINUTES_PAR_JOURNEE = 24 * 60
 
 
 def tarif(duree_en_minutes):
     minutes_facturables = max(0, duree_en_minutes - MINUTES_GRATUITES_STANDARD)
     montant = math.ceil(minutes_facturables / MINUTES_PAR_TRANCHE) * TARIF_PAR_TRANCHE
-    return min(montant, PLAFOND_PAR_JOURNEE)
+    journees = max(1, math.ceil(duree_en_minutes / MINUTES_PAR_JOURNEE))
+    return min(montant, journees * PLAFOND_PAR_JOURNEE)
